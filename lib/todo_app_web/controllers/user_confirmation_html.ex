52c095e6810487e84a8456bdc6cd8b12a7bb007f
Emmy Achieng <emmy@investbamboo.com>
2023-09-17 11:33:22 +0300

    added user authentication

---
+++ lib/todo_app_web/controllers/user_confirmation_html.ex
@@ -0,0 +1,5 @@
+defmodule TodoAppWeb.UserConfirmationHTML do
+  use TodoAppWeb, :html
+
+  embed_templates "user_confirmation_html/*"
+end
---
+++ lib/todo_app_web/controllers/user_session_html.ex
@@ -0,0 +1,5 @@
+defmodule TodoAppWeb.UserSessionHTML do
+  use TodoAppWeb, :html
+
+  embed_templates "user_session_html/*"
+end
